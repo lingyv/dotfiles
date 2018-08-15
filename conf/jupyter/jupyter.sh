5ead7c9a55f31230452648f95675eb0a2b1589aa
lingyv <lingyv@gmail.com>
2018-08-15 15:26:59 +0800

    添加jupyter配置文件

---
+++ conf/jupyter/jupyter.sh
@@ -0,0 +1,5 @@
+pip install --upgrade jupyterthemes
+pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+jt -t oceans16 -altp -dfs 10 -ofs 10 -cellw 88% -T -f fira
+
