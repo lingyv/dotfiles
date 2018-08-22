ae2f9f358ed7b320d622f6f1568f63975d4418ba
lingyv <lingyv@gmail.com>
2018-08-22 19:20:00 +0800

    软链接 neovim 配置文件

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,8 @@ function doIt() {
     sudo ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig
     sudo ln -s -f ~/dotfiles/.gdbinit ~/.gdbinit
     sudo ln -s -f ~/dotfiles/.vimrc ~/.vimrc
+    sudo ln -s -f ~/.vim ~/.config/nvim
+    sudo ln -s -f ~/dotfiles/.vimrc ~/.config/nvim/init.vim
     sudo ln -s -f ~/dotfiles/.ideavimrc ~/.ideavimrc
     sudo ln -s -f ~/dotfiles/.exports ~/.exports
     sudo ln -s -f ~/dotfiles/.tern-project ~/.tern-project
